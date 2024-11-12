2b48fb7da0c8a4fc6d19597471769a969ee4b0d6
li <2633290579@qq.com>
2024-11-12 18:46:20 +0800

    init&简单翻译_ok

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+   <LL />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import LL from './components/LL.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LL
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://fanyi-api.baidu.com',  // 目标 API 地址
+        changeOrigin: true,  // 修改源
+        pathRewrite: { '^/api': '' },  // 重写路径
+        secure: false,
+      },
+    },
+  },
+
 })
